82edda08efc141048e0b73c2cad9072b0383b6eb
Jonathan Hornung <jonathan.hornung@gmail.com>
2016-01-06 15:15:17 +0100

    chore: create non-minified dist file (#1)

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,21 +12,33 @@ module.exports = function(grunt) {
                 banner: '\n/*! <%= pkg.name %> v<%= pkg.version %> (<%= grunt.template.today("dd-mm-yyyy") %>) by <%= pkg.author %> */\n',
             }
         },
+        concat: {
+            options: {
+                separator: ';',
+                banner: '\n/*! <%= pkg.name %> v<%= pkg.version %> (<%= grunt.template.today("dd-mm-yyyy") %>) by <%= pkg.author %> */\n',
+            },
+            dist: {
+                files : {
+                    'dist/angular-bandsintown-api-factory.js' : ['src/angular-bandsintown-api-factory.js']
+                }
+            },
+        },
         watch: {
             minifiyJs: {
                 files: [
                     'src/angular-bandsintown-api-factory.js'
                 ],
-                tasks: ['uglify'],
+                tasks: ['uglify', 'concat'],
                 options: {
                     spawn: true,
                 },
-            },
-        },
+            }
+        }
     });
 
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-concat');
 
     grunt.registerTask('default', ['watch']);
 
---
+++ dist/angular-bandsintown-api-factory.js
@@ -0,0 +1,136 @@
+
+/*! angular-bandsintown-api-factory v0.5.0 (06-01-2016) by Jonathan Hornung */
+"use strict";
+
+/**
+ @author Jonathan Hornung (https://github.com/JohnnyTheTank)
+ @url https://github.com/JohnnyTheTank/angular-bandsintown-api-factory
+ @licence MIT
+ */
+
+angular.module("jtt_bandsintown", [])
+    .factory('bandsintownFactory', ['$http', 'bandsintownSearchDataService', function ($http, bandsintownSearchDataService) {
+
+        var bandsintownFactory = {};
+
+        bandsintownFactory.getArtist = function (_params) {
+            var bandsintownSearchData = bandsintownSearchDataService.getNew("artist", _params);
+
+            return $http.jsonp(
+                bandsintownSearchData.url,
+                {
+                    method: 'GET',
+                    params: bandsintownSearchData.object,
+                }
+            );
+        };
+
+        bandsintownFactory.getEventsFromArtist = function (_params) {
+            var bandsintownSearchData = bandsintownSearchDataService.getNew("eventsFromArtist", _params);
+
+            return $http.jsonp(
+                bandsintownSearchData.url,
+                {
+                    method: 'GET',
+                    params: bandsintownSearchData.object,
+                }
+            );
+        };
+
+        bandsintownFactory.getEventsFromArtistByLocation = function (_params) {
+            var bandsintownSearchData = bandsintownSearchDataService.getNew("eventsFromArtistByLocation", _params);
+
+            return $http.jsonp(
+                bandsintownSearchData.url,
+                {
+                    method: 'GET',
+                    params: bandsintownSearchData.object,
+                }
+            );
+        };
+
+        bandsintownFactory.getRecommendedEventsFromArtistByLocation = function (_params) {
+            var bandsintownSearchData = bandsintownSearchDataService.getNew("recommendedEventsFromArtistByLocation", _params);
+
+            return $http.jsonp(
+                bandsintownSearchData.url,
+                {
+                    method: 'GET',
+                    params: bandsintownSearchData.object,
+                }
+            );
+        };
+
+        return bandsintownFactory;
+    }])
+    .service('bandsintownSearchDataService', function () {
+        this.getApiBaseUrl = function (_params) {
+            return 'http://api.bandsintown.com/';
+        };
+
+        this.fillDataInObjectByList = function(_object, _params, _list) {
+
+            angular.forEach(_list, function (value, key) {
+                if(typeof _params[value] !== "undefined") {
+                    _object.object[value] = _params[value];
+                }
+            });
+
+            return _object;
+        };
+
+        this.getNew = function (_type, _params) {
+
+            var bandsintownSearchData = {
+                object: {
+                    format:"json",
+                    api_version: "2.0",
+                    app_id:_params.app_id || "angular-bandsintown-api-factory",
+                    callback: "JSON_CALLBACK",
+                },
+                url: "",
+            };
+
+            if (typeof _params.limit !== "undefined") {
+                bandsintownSearchData.object.limit = _params.limit;
+            }
+
+            switch (_type) {
+
+                case "artist":
+                    bandsintownSearchData = this.fillDataInObjectByList(bandsintownSearchData, _params, [
+                        'artist_id'
+                    ]);
+
+
+                    bandsintownSearchData.url = this.getApiBaseUrl()+"artists/"+_params.artist+".json";
+                    break;
+
+                case "eventsFromArtist":
+                    bandsintownSearchData = this.fillDataInObjectByList(bandsintownSearchData, _params, [
+                        'date', 'artist_id'
+                    ]);
+
+                    bandsintownSearchData.url = this.getApiBaseUrl()+"artists/"+_params.artist+"/events.json";
+                    break;
+
+                case "eventsFromArtistByLocation":
+                    bandsintownSearchData = this.fillDataInObjectByList(bandsintownSearchData, _params, [
+                        'date', 'artist_id', 'location', 'radius'
+                    ]);
+
+                    bandsintownSearchData.url = this.getApiBaseUrl()+"artists/"+_params.artist+"/events/search.json";
+                    break;
+
+                case "recommendedEventsFromArtistByLocation":
+                    bandsintownSearchData = this.fillDataInObjectByList(bandsintownSearchData, _params, [
+                        'date', 'artist_id', 'location', 'radius', 'only_recs'
+                    ]);
+
+                    bandsintownSearchData.url = this.getApiBaseUrl()+"artists/"+_params.artist+"/events/recommended";
+                    break;
+            }
+
+            return bandsintownSearchData;
+        };
+    });
